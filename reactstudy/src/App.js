eb96e6ae1d8189433e6c25694c15b870e1395612
jeonghyun06 <roskrlss85@gmail.com>
2023-08-29 00:08:57 +0900

    create toDo list

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,20 +1,43 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((current) => [...current, toDo]);
+    setToDo("");
+  };
+  const deleteTodo = (index) => {
+    setToDos(toDos.filter((_, curIndex) => curIndex !== index));
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your todo..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <div key={index}>
+          <li key={index}>
+            {item}
+            <button key={index} onClick={() => deleteTodo(index)}>
+              delete to do
+            </button>
+          </li>
+        </div>
+      ))}
     </div>
   );
 }
